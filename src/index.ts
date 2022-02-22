8148fe8359a1aa3fba3945b0a32730fac2ce028e
Artem Cherniy <artemcherniy7@gmail.com>
2022-02-22 08:53:40 +0000

    fix: fix and handle errors

--- src/index.ts
+++ src/index.ts
@@ -46,85 +46,89 @@ const randomIntFromInterval = (min: number, max: number): number =>
 
 // Scraping and buying
 const getBookWithPuppeteer = async (pickedGenre: string) => {
-  // Set up puppeteer
-  const browser = await puppeteer.launch({ headless: false });
-  const page = await browser.newPage();
-  const URL = `https://www.goodreads.com/choiceawards/best-${genreToUrl(
-    pickedGenre
-  )}-books-2020`;
-  console.log(URL);
-  const amazonPage = 'https://www.amazon.com/';
+  try {
+    // Set up puppeteer
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    const URL = `https://www.goodreads.com/choiceawards/best-${genreToUrl(
+      pickedGenre
+    )}-books-2020`;
+    const amazonPage = 'https://www.amazon.com/';
 
-  // Page viewport
-  await page.setViewport({
-    width: 1280,
-    height: 800,
-  });
-  await page.goto(URL, { waitUntil: 'networkidle2' });
-  // Get array of book
-  const titles = await page.evaluate(() =>
-    Array.from(document.querySelectorAll('.pollAnswer__bookLink')).map(
-      (title) => title.getElementsByTagName('img')[0].getAttribute('alt')
-    )
-  );
+    // Page viewport
+    await page.setViewport({
+      width: 1280,
+      height: 800,
+    });
+    await page.goto(URL, { waitUntil: 'networkidle2' });
+    // Get array of book
+    const titles = await page.evaluate(() =>
+      Array.from(document.querySelectorAll('.pollAnswer__bookLink')).map(
+        (title) => title.getElementsByTagName('img')[0].getAttribute('alt')
+      )
+    );
 
-  // Getting random books from https://www.goodreads.com/choiceawards/best-books-2020
-  const randomBookOfDesireGenre =
-    titles[randomIntFromInterval(1, titles.length)];
+    // Getting random books from https://www.goodreads.com/choiceawards/best-books-2020
+    const randomBookOfDesireGenre =
+      titles[randomIntFromInterval(1, titles.length)];
 
-  // Change page to amazon
-  await page.goto(amazonPage, { waitUntil: 'networkidle2' });
+    // Change page to amazon
+    await page.goto(amazonPage, { waitUntil: 'networkidle2' });
 
-  //Enter Search criteria
-  const searchBox = await page.waitForXPath("//*[@id='twotabsearchtextbox']", {
-    visible: true,
-  });
-  if (searchBox !== null && searchBox)
-    await searchBox.type(randomBookOfDesireGenre);
+    //Enter Search criteria
+    const searchBox = await page.waitForXPath(
+      "//*[@id='twotabsearchtextbox']",
+      {
+        visible: true,
+      }
+    );
+    if (searchBox !== null && searchBox && randomBookOfDesireGenre)
+      await searchBox.type(randomBookOfDesireGenre);
 
-  // Clicked on search button
-  const searchHandler = await page.waitForXPath(
-    "//*/input[@id='nav-search-submit-button']",
-    { visible: true }
-  );
-  if (searchHandler !== null && searchHandler) await searchHandler.click();
+    // Clicked on search button
+    const searchHandler = await page.waitForXPath(
+      "//*/input[@id='nav-search-submit-button']",
+      { visible: true }
+    );
+    if (searchHandler !== null && searchHandler) await searchHandler.click();
 
-  // Click on first search result
-  const getBookFromList = await page.waitForXPath(
-    '//*[@id="search"]/div[1]/div[1]/div/span[3]/div[2]/div[2]/div/div/div/div/div/div[1]/div/div[2]/div/span/a',
-    { visible: true }
-  );
-  if (getBookFromList !== null && getBookFromList)
-    await getBookFromList.click();
+    // Click on first search result
+    const getBookFromList = await page.waitForXPath(
+      '//*[@id="search"]/div[1]/div[1]/div/span[3]/div[2]/div[2]/div/div/div/div/div/div[1]/div/div[2]/div/span/a',
+      { visible: true }
+    );
+    if (getBookFromList !== null && getBookFromList)
+      await getBookFromList.click();
 
-  // Pick paperback book to be able to add it to card
-  await page.waitForXPath('//a/span[contains(text(), "Paperback")]');
-  const paperBackLink = await page.$x(
-    '//a/span[contains(text(), "Paperback")]'
-  );
-  if (paperBackLink.length > 0) {
-    await paperBackLink[0].click();
-  }
-
-  // Add to cart
-  const addToCart = await page.waitForXPath('//*[@id="add-to-cart-button"]', {
-    visible: true,
-  });
-  if (addToCart !== null) await addToCart.click();
-
-  // Checkout
-  const checkoutHandler = await page.waitForXPath(
-    '//*[@id="sc-buy-box-ptc-button"]/span/input',
-    {
-      visible: true,
+    // Pick paperback book to be able to add it to card
+    await page.waitForXPath('//a/span[contains(text(), "Paperback")]');
+    const paperBackLink = await page.$x(
+      '//a/span[contains(text(), "Paperback")]'
+    );
+    if (paperBackLink.length > 0 && paperBackLink) {
+      await paperBackLink[0].click();
     }
-  );
-  if (checkoutHandler !== null && checkoutHandler)
-    await checkoutHandler.click();
 
-  await page.waitForTimeout(10000);
-  //   Closing browser
-  await browser.close();
+    // Add to cart
+    const addToCart = await page.waitForXPath('//*[@id="add-to-cart-button"]', {
+      visible: true,
+    });
+    if (addToCart !== null && addToCart) await addToCart.click();
+
+    // Checkout
+    const checkoutHandler = await page.waitForXPath(
+      '//*[@id="sc-buy-box-ptc-button"]/span/input',
+      {
+        visible: true,
+      }
+    );
+    if (checkoutHandler !== null && checkoutHandler)
+      await checkoutHandler.click();
+    //   Closing browser
+    await browser.close();
+  } catch (error) {
+    console.error(error);
+  }
 };
 
 // Error handling
