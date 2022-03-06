fc355b5c07ef1b467c96dbe4ca8f0124b0788e0a
Artem Cherniy <artemcherniy7@gmail.com>
2022-03-06 14:06:50 +0000

    feat: add fetching categories for specific year

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 // Imports
 const inquirer = require('inquirer');
 const puppeteer = require('puppeteer');
-import { genres } from './genres';
 
-// Convert genre into url
-const genreToUrl = (genre: string) => {
+// vars
+const years: string[] = ['2011', '2012', '2021'];
+
+// Convert genre into url (now this functions never used)
+const genreToUrl = (genre: string): string => {
   return genre.split(' ').length === 0
     ? genre.toLowerCase().trim()
     : genre
@@ -21,15 +23,62 @@ const getBookByGenre = (): void => {
     .prompt([
       {
         type: 'list',
-        name: 'genre',
-        message: 'Which genre do you prefer? 😁',
-        choices: genres,
+        name: 'year',
+        message: 'Pick a year',
+        choices: years,
       },
     ])
-    .then((answers: { genre: string }) => {
-      const pickedGenre = answers.genre;
-      if (pickedGenre) getBookWithPuppeteer(pickedGenre);
+    .then((answers: { year: string }) => {
+      const year = answers;
+      fetchCategories(year.year).then((res) => {
+        const categories = res;
+        pickCategory(categories);
+      });
     });
+};
+
+// Pick category function where we scrap categories for specific year and add them as options
+const pickCategory = (categories: { category: string; url: string }[]) => {
+  inquirer
+    .prompt([
+      {
+        type: 'list',
+        name: 'genre',
+        message: 'Which genre do you prefer? 😁',
+        choices: categories.map((cat) => cat.category),
+      },
+    ])
+    .then((answer: { genre: string }) => {
+      const category = categories.filter(
+        (cat) => cat.category === answer.genre
+      );
+      getBookWithPuppeteer(category[0]);
+    });
+};
+
+// fetch categories
+const fetchCategories = async (year: string) => {
+  try {
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+    const URL = `https://www.goodreads.com/choiceawards/best-books-${year}`;
+    await page.setViewport({
+      width: 1280,
+      height: 800,
+    });
+    await page.goto(URL, { waitUntil: 'networkidle2' });
+    const categories = await page.evaluate(() =>
+      Array.from(document.querySelectorAll('.category > a')).map((cat) => {
+        return {
+          category: cat.childNodes[0].textContent,
+          url: cat.getAttribute('href'),
+        };
+      })
+    );
+    return categories;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 // Bootstrap
@@ -45,14 +94,15 @@ const randomIntFromInterval = (min: number, max: number): number =>
   Math.floor(Math.random() * (max - min + 1) + min);
 
 // Scraping and buying
-const getBookWithPuppeteer = async (pickedGenre: string) => {
+const getBookWithPuppeteer = async (category: {
+  category: string;
+  url: string;
+}) => {
   try {
     // Set up puppeteer
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
-    const URL = `https://www.goodreads.com/choiceawards/best-${genreToUrl(
-      pickedGenre
-    )}-books-2020`;
+    const URL = `https://www.goodreads.com/choiceawards/${category.url}`;
     const amazonPage = 'https://www.amazon.com/';
 
     // Page viewport
@@ -132,7 +182,7 @@ const getBookWithPuppeteer = async (pickedGenre: string) => {
 };
 
 // Error handling
-const logErrorAndExit = (err: Error) => {
+const logErrorAndExit = (err: Error): void => {
   console.log(err);
   process.exit();
 };
